253844cf1539bb43b2204f2e78bd7b6bf6974a2b
kavin <kavinnishanthan2004@gmail.com>
2024-09-09 15:34:27 +0530

    drive route update

--- src/routes/resume.route.ts
+++ src/routes/resume.route.ts
@@ -8,15 +8,15 @@ import resumeController from '../controllers/resume.comtrollers';
 const router = Router();
 
 // Resume routes
-router.post('/create-resume/:collegeId/:studentId/', resumeController.createResume);
-router.get('/get-resume/:studentId', resumeController.getResume);
-router.put('/update-resume/:studentId', resumeController.updateResume);
 router.post('/add-skill/:studentId', resumeController.addSkills);
+router.get('/get-resume/:studentId', resumeController.getResume);
 router.post('/add-project/:studentId', resumeController.addProject);
-router.delete('/delete-project/:studentId/:projectId', resumeController.deleteProject);
+router.put('/update-resume/:studentId', resumeController.updateResume);
 router.post('/add-experience/:studentId', resumeController.addExperience);
-router.delete('/delete-experience/:studentId/:experienceId', resumeController.deleteExperience);
 router.post('/add-interest/:studentId', resumeController.addAreaOfInterest);
+router.post('/create-resume/:collegeId/:studentId/', resumeController.createResume);
 router.patch('/delete-interest/:studentId/', resumeController.deleteAreaOfInterest);
+router.delete('/delete-project/:studentId/:projectId', resumeController.deleteProject);
+router.delete('/delete-experience/:studentId/:experienceId', resumeController.deleteExperience);
 
 export default router;
